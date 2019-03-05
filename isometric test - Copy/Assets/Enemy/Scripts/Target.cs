a2a6a73338ebae990ee2b9eac526713679668bda
Marz Pimentel <marzmail@gmail.com>
2019-03-05 12:50:24 -0800

    Here is the isometric test folder

---
+++ isometric test - Copy/Assets/Enemy/Scripts/Target.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int health = 100; 
+
+    public void TakeDamage(int Amount) {
+        health -= Amount;
+        if (health <= 0)
+        {
+            Destroy();
+        }
+    }
+
+    void Destroy() {
+        Destroy(this.gameObject);
+    }
+}
