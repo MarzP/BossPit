e07ba6e8c3b5780d436ac91b99ba109e01ebf833
Aria <maria.zuk@icloud.com>
2019-04-30 22:53:07 -0700

    Pause/Restart

---
+++ TheVault/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+  public void RestartGame()
+  {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      Resume();
+  }
+  public void QuitGame()
+  {
+      Debug.Log("QUIT!");
+      Application.Quit();
+  }
+}
