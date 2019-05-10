119e84b61c788bacaac4dabb634c0f570bccc830
Marz Pimentel <marzmail@gmail.com>
2019-05-09 22:32:08 -0700

    making really shitty things that dont work

---
+++ TheVault/Assets/moveSphere.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveSphere : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private GameObject boss;
+
+    public float speed;
+    public float destroyTime;
+    public int damage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss = GameObject.FindWithTag("Enemy");
+        rb = GetComponent<Rigidbody>();
+        Physics.IgnoreLayerCollision(12, 12, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 center = transform.position;
+        Vector3 direction = (transform.position - center).normalized;
+        transform.position += direction * speed;
+        
+        Destroy(this.gameObject, destroyTime);
+    }
+}
